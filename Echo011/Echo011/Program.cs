330959094c3c1b10fda2881d23827437beb008eb
kuertian <kuertian@qq.com>
2020-11-12 17:18:35 +0800

    Echo011 实例011实现简单计算器--算术运算

---
+++ Echo011/Echo011/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数：");
+            int Number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入算术运算符：");
+            String Operator = Console.ReadLine();
+            Console.WriteLine("请输入第二个数：");
+            int Number2 = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            switch(Operator)
+            {
+                case "+":
+                    result = Number1;
+                    result += Number2;
+                    break;
+                case "-":
+                    result = Number1 - Number2;
+                    break;
+                case "*":
+                    result = Number1 * Number2;
+                    break;
+                case "/":
+                    result = Number1 / Number2;
+                    break;
+                case "%":
+                    result = Number1 % Number2;
+                    break;
+                default:
+                    Console.WriteLine("运算符输入错误！");
+                    return;
+            }
+            Console.WriteLine("{0} {1} {2} = {3}", Number1, Operator, Number2, result);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Example-of-programming
+﻿# Example-of-programming
 
 Echo008
 实例008 修改圆柱体的高度和底面半径--引用类型
+
+Echo009
+实例010计算矩形的面积--变量
+
+Echo011 
+实例011实现简单计算器--算术运算
+
+
