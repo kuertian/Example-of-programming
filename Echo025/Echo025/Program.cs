abf8a8e247e2d3e168fb3cd3dd49dd91f3b18c23
kuertian <kuertian@qq.com>
2020-11-14 00:13:18 +0800

    Echo025 实例025 输出1到20之间的偶数--do while循环语句

---
+++ Echo025/Echo025/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                if (++i % 2 != 0)
+                    continue;
+                Console.Write(i);
+                if (i % 5 == 0)
+                    Console.WriteLine();
+                else
+                    Console.Write("\t");
+            } while (i < 20);
+
+            Console.ReadLine();
+        }
+    }
+}
