7267403c86bd22709369037c0948b0bdb8f95ec3
kuertian <kuertian@qq.com>
2020-11-14 00:04:01 +0800

    Echo024 实例024 输出1到20之间的整数--while循环语句

---
+++ Echo024/Echo024/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (true)
+            {
+                Console.Write(i);
+                if (i % 5 == 0)     //每5个数换行输出
+                    Console.WriteLine();
+                else
+                    Console.Write("\t");
+                if (++i > 20)       //当i大于20时跳出循环
+                    break;
+            }
+            Console.ReadLine();
+        }        
+    }
+}
