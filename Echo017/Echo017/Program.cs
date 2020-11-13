e5f651cdf1a7bcd83b7cefe4f7be8cddd5528769
kuertian <kuertian@qq.com>
2020-11-13 20:09:27 +0800

    Echo017 实例017 求解正整数的阶乘--函数的递归调用及值传递

---
+++ Echo017/Echo017/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo017
+{
+    class Program
+    {
+        static decimal Factorial(decimal n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数：");
+            decimal number = decimal.Parse(Console.ReadLine());
+            decimal fac = Factorial(number);
+            Console.WriteLine("{0}的阶乘为{1}", number,fac);
+            Console.ReadLine();
+        }
+    }
+}
