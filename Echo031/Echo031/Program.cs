6936b8c5b1ac81688f0659638cff52a467b60f4b
kuertian <kuertian@qq.com>
2020-11-14 13:37:18 +0800

    Echo031 实例031 存储多个学生姓名--一维数组

---
+++ Echo031/Echo031/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[6] { "张三", "李四", "王五", "赵六", "孙七", "周八" };
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i] + "\t");            
+
+            }
+
+            Console.WriteLine("\n将 {0} 改名为 {1}", "周八", "吴九");
+            students[5] = "吴九";
+
+            foreach (string name in students)
+            {
+                Console.Write(name + "\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
