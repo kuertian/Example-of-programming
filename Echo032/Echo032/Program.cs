2a5c9dcb1c294022faca929361f064014f5f6052
kuertian <kuertian@qq.com>
2020-11-14 14:13:47 +0800

    Echo032 实例032 生成学生成绩单--创建二维数组

---
+++ Echo032/Echo032/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo032
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[,] results = new byte[20, 4];
+            Random random = new Random();
+            Console.WriteLine("学号\t高数\tC语言\t英语");
+
+            for (int id = 0; id < results.GetLength(0); id++)
+            {
+                Console.Write(id);
+                for (int i = 1; i < results.GetLength(1); i++)
+                {
+                    results[id, i] = (byte)random.Next(101); //随机生成学生成绩
+                    Console.Write("\t" + results[id, i]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
