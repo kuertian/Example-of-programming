bd9da609cd773f8e634dd5fd98d85bae3b11f826
kuertian <kuertian@qq.com>
2020-11-14 10:15:23 +0800

    Echo027 实例027 用符号*排列成三角形--for循环嵌套调用

---
+++ Echo027/Echo027/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo027
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 10; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+               
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("* ");
+                }
+                
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
