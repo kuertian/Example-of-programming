fe9f82fb8c238d57d145b7ec37ddbee65fb1d96d
kuertian <kuertian@qq.com>
2020-11-14 10:47:12 +0800

    Echo029 实例029 输出字符串中的每个字符--foreach循环语句

---
+++ Echo029/Echo029/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入字符串：");
+            string String = Console.ReadLine();
+
+            foreach (Char c in String)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
