2c050038c4c9a80f85ed7c3f708b2b04d8270c68
kuertian <kuertian@qq.com>
2020-11-17 10:46:26 +0800

    Echo042 实例042 将古诗分行输出--System.Char类型

---
+++ Echo042/Echo042/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string poetry = "日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。\n";
+            Console.WriteLine(poetry);
+            foreach (char item in poetry)
+            {
+                Console.Write(item);
+                Console.Write("\t");
+                if (Char.IsPunctuation(item))
+                    Console.Write("\n\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
